76ba0847256986bd16efa488b2e5ed6cce58d75f
andornaut <andornaut@users.noreply.github.com>
2026-06-13 14:14:51 -0400

    Label v4 TRRS protection parts descriptively
    TVS (sod-123fl): name pads K (cathode/'to') and A (anode/'from') instead
    of 1/2, set the value to SMF3.3CA, and document the params/pads in comments.
    Resistor (smd_0805): override designator to R (R1, not the generic SMD1)
    and add a '100R' silk value label. Document each part's pad->net mapping
    in the ergogen config comments.

--- ergogen/footprints/sod-123fl.js
+++ ergogen/footprints/sod-123fl.js
@@ -1,8 +1,12 @@
+// Custom SOD-123FL flat-lead footprint for the Littelfuse SMF3.3CA data-line TVS.
+// Reversible plated-through hand-solder pads (present on both copper sides).
+// Geometry matches the Nexperia SOD123FL recommended land (pitch 3.2mm, body
+// 3.0 x 1.85mm) adapted to through-hole. See ../../AGENTS.md "TRRS Data-line Protection".
 module.exports = {
     params: {
-        designator: 'D',
-        from: {type: 'net', value: 'from'},
-        to: {type: 'net', value: 'to'}
+        designator: 'D',                       // reference prefix -> D<n>
+        from: {type: 'net', value: 'from'},    // anode  -> pad "A"
+        to: {type: 'net', value: 'to'}         // cathode (banded end) -> pad "K"
     },
     body: p => `
         (footprint "splinter:D_SOD-123FL_HandSolder"
@@ -13,7 +17,7 @@ module.exports = {
             (fp_text reference "${p.ref}" (at 0 -2 ${p.rot}) (layer "F.SilkS") ${p.ref_hide}
                 (effects (font (size 0.8 0.8) (thickness 0.12)))
             )
-            (fp_text value "SOD-123FL" (at 0 2 ${p.rot}) (layer "F.Fab") hide
+            (fp_text value "SMF3.3CA" (at 0 2 ${p.rot}) (layer "F.Fab") hide
                 (effects (font (size 0.8 0.8) (thickness 0.12)))
             )
 
@@ -43,8 +47,11 @@ module.exports = {
             (fp_line (start 2.4 1.15) (end -2.4 1.15) (stroke (width 0.05) (type solid)) (layer "B.CrtYd"))
             (fp_line (start -2.4 1.15) (end -2.4 -1.15) (stroke (width 0.05) (type solid)) (layer "B.CrtYd"))
 
-            (pad "1" thru_hole rect (at -1.6 0 ${p.rot}) (size 1.1 1.35) (drill 0.3) (layers *.Cu *.Mask) (zone_connect 2) ${p.to.str})
-            (pad "2" thru_hole rect (at 1.6 0 ${p.rot}) (size 1.1 1.35) (drill 0.3) (layers *.Cu *.Mask) (zone_connect 2) ${p.from.str})
+            ${''/* Pad "K" = cathode (banded end) = 'to' net; pad "A" = anode = 'from' net.
+                  SMF3.3CA is bidirectional so orientation is electrically irrelevant; for a
+                  unidirectional diode, K must face the more-positive net. */}
+            (pad "K" thru_hole rect (at -1.6 0 ${p.rot}) (size 1.1 1.35) (drill 0.3) (layers *.Cu *.Mask) (zone_connect 2) ${p.to.str})
+            (pad "A" thru_hole rect (at 1.6 0 ${p.rot}) (size 1.1 1.35) (drill 0.3) (layers *.Cu *.Mask) (zone_connect 2) ${p.from.str})
         )
     `
 }
